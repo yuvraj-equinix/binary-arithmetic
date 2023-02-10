fe9d7b97d797a7c4cad4d6c7d8f7429a30ce3d32
yuvraj-equinix <ymishra@equinix.com>
2023-02-10 13:52:52 +0530

    partial conversion

--- binary_arithmetic.py
+++ binary_arithmetic.py
@@ -4,14 +4,23 @@ class Binary:
         self.b = decimal_to_binary(b)
     
     def add(self, a, b):
+        pass
         
     def sub(self, a, b):
+        pass
         
     def mul(self, a, b):
+        pass
         
     def div(self, a, b):
+        pass
         
-    def decimal_to_binary(self, a, b):
+    def decimal_to_binary(self, num):
+        bin_a = [0] * 32
+        bin_b = [0] * 32
+        for i in range(31, -1, -1):
+            bin_a[i] = num % 2
+            # todo
         
-    def binary_to_decimal(self, a, b):
-        
+    def binary_to_decimal(self, arr):
+        pass
