b71c0fa6090252d2d52f431f51e8621b61ce51b6
yuvraj-equinix <ymishra@equinix.com>
2023-02-10 15:29:36 +0530

    implement multiplication

--- binary_arithmetic.py
+++ binary_arithmetic.py
@@ -4,12 +4,20 @@ def decimal_to_binary(num):
         bin[i] = num % 2
         num = num // 2
     return bin
-    
+
+
 def binary_to_decimal(arr):
+    is_negative = False
+    if arr[0] == 1:
+        is_negative = True
+        arr = complement_2(arr)
     num = 0
-    for i in range(32):
-        num = num + arr[i] * 2**(31-i)
+    for i in range(1, 32):
+        num = num + arr[i] * 2 ** (31 - i)
+    if is_negative:
+        return -num
     return num
+
 
 def complement_2(arr):
     ans = arr[:]
@@ -17,34 +25,48 @@ def complement_2(arr):
         ans[i] = 0 if arr[i] == 1 else 1
     return add(ans, decimal_to_binary(1))
 
+
 def add(a, b):
-    ans = [0]*32
+    ans = [0] * 32
     carry, i = 0, 31
     while i >= 0:
         sum = a[i]
         sum += b[i]
         sum += carry
-        
-        ans[i] = sum%2
-        carry = sum//2
-    
+
+        ans[i] = sum % 2
+        carry = sum // 2
+
         i -= 1
     return ans
-    
+
+
 def sub(a, b):
     c = complement_2(b)
     ans = add(a, c)
     return ans
-    
+
+
 def mul(a, b):
-    pass
-    
+    is_negative = False
+    a_decimal = binary_to_decimal(a)
+    b_decimal = binary_to_decimal(b)
+    if a_decimal < 0 and b_decimal > 0 or b_decimal < 0 and a_decimal > 0:
+        is_negative = True
+    ans = [0] * 32
+    for i in range(abs(b_decimal)):
+        ans = add(ans, a)
+    if is_negative:
+        return complement_2(ans)
+    return ans
+
+
 def div(a, b):
     pass
-    
 
 
-a = decimal_to_binary(-6)
-b = decimal_to_binary(-4)
-ans = sub(a, b)
+a = decimal_to_binary(3)
+b = decimal_to_binary(-5)
+ans = mul(a, b)
 print(ans)
+print(binary_to_decimal(ans))
