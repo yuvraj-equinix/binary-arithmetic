76e1536e3b15b8d2e1b692a4c59729c91b6bc915
yuvraj-equinix <ymishra@equinix.com>
2023-02-10 13:41:50 +0530

    add class layout

---
+++ binary_arithmetic.py
@@ -0,0 +1,17 @@
+class Binary:
+    def __init__(self, a, b):
+        self.a = decimal_to_binary(a)
+        self.b = decimal_to_binary(b)
+    
+    def add(self, a, b):
+        
+    def sub(self, a, b):
+        
+    def mul(self, a, b):
+        
+    def div(self, a, b):
+        
+    def decimal_to_binary(self, a, b):
+        
+    def binary_to_decimal(self, a, b):
+        
