4ac42029bf96c51aa1a803a8abbc85dec8ca4785
h-sumeet <shaldar@equinix.com>
2023-02-11 12:50:28 +0530

    implement factorial

--- binary_arithmetic.py
+++ binary_arithmetic.py
@@ -86,32 +86,28 @@ def factorial(bin):
     
     return decimal_to_binary(ans)
 
-a = decimal_to_binary(4)
-ans = factorial(a)
-print(ans)
+while True:
+    print("Enter the First Number")
+    num1 = int(input())
+    bin1 = decimal_to_binary(num1)
+    print("Enter the Second Number")
+    num2 = int(input())
+    bin2 = decimal_to_binary(num2)
+    print("Enter the Operator")
+    operator = input()
 
-# while True:
-#     print("Enter the First Number")
-#     num1 = int(input())
-#     bin1 = decimal_to_binary(num1)
-#     print("Enter the Second Number")
-#     num2 = int(input())
-#     bin2 = decimal_to_binary(num2)
-#     print("Enter the Operator")
-#     operator = input()
-
-#     if operator == '+':
-#         ans = add(bin1, bin2)
-#         print(binary_to_decimal(ans))
-#     elif operator == '-':
-#         ans = sub(bin1, bin2)
-#         print(binary_to_decimal(ans))
-#     elif operator == '*':
-#         ans = mul(bin1, bin2)
-#         print(binary_to_decimal(ans))
-#     elif operator == '/':
-#         ans = div(bin1, bin2)
-#         print(binary_to_decimal(ans))
-#     else:
-#         print("Invalid Input")
+    if operator == '+':
+        ans = add(bin1, bin2)
+        print(binary_to_decimal(ans))
+    elif operator == '-':
+        ans = sub(bin1, bin2)
+        print(binary_to_decimal(ans))
+    elif operator == '*':
+        ans = mul(bin1, bin2)
+        print(binary_to_decimal(ans))
+    elif operator == '/':
+        ans = div(bin1, bin2)
+        print(binary_to_decimal(ans))
+    else:
+        print("Invalid Input")
     
